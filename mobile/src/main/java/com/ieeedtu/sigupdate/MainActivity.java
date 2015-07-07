904331b4115285008ccf2205df577fb7124f003c
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-07-07 19:01:37 +0530

    added recyclerview

--- mobile/src/main/java/com/ieeedtu/sigupdate/MainActivity.java
+++ mobile/src/main/java/com/ieeedtu/sigupdate/MainActivity.java
@@ -1,16 +1,23 @@
 package com.ieeedtu.sigupdate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(llm);
     }
 
     @Override
