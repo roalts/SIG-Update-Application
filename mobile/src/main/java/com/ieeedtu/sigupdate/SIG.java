09765e0277c3a8d14a379a414ed5f235f267ea6a
Raghav Apoorv <raghav.apoorv@gmail.com>
2015-07-07 19:02:50 +0530

    created sig pojo

---
+++ mobile/src/main/java/com/ieeedtu/sigupdate/SIG.java
@@ -0,0 +1,43 @@
+package com.ieeedtu.sigupdate;
+
+/**
+ * Created by raghav on 07/07/15.
+ */
+public class SIG {
+    private String name;
+    private String time;
+    private String topic;
+    private String room;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+}
